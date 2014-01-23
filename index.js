0358e57ccec4614fe6d98ff5dac80654ce387c73
killercodemonkey <bengtler@gmail.com>
2014-01-23 20:09:22 +0100

    remove static image file

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ var productMock = {
     'price': '123,45 €',
     'name': 'Fancy Product',
     'description': 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet',
-    'image': '../images/fancyproduct.jpg',
     'instock': true
 };
 
