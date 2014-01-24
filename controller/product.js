36b19e26ea4e160fbe3a4fae4cbaefa7723edafc
Martin Pfeiffer <martinp.dev@googlemail.com>
2014-01-24 12:44:22 +0100

    implement a routing table for a simple url schema

--- controller/product.js
+++ controller/product.js
@@ -1,7 +1,11 @@
-var productMock = '{"price": 123.45,"name": "Fancy Product","description": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet","instock": true}';
+productdata = {
+	'1': '{"price": 123.45,"name": "Fancy Product","description": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet","instock": true}',
+	'2': '{"price": 12.45,"name": "Fancy Product II","description": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet","instock": false}'
+};
 
-exports.getData = function () {
-	var product = JSON.parse(productMock);
+exports.getData = function (id) {
+	var data = productdata[id] || '{}';
+	var product = JSON.parse(data);
 	product.price = product.price + ' €';
 	return product;
 };
--- index.js
+++ index.js
@@ -3,14 +3,46 @@ var MustacheEngine = require('mu2'); // mu2 is a fast mustache engine
 
 MustacheEngine.root = 'templates';
 
-var template = 'product.html';
+var producttemplate = 'product.html';
 var productcontroller = require('./controller/product');
 
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-    var stream = MustacheEngine.compileAndRender(template, productcontroller.getData());
+var routes = {
+    'test1': {
+        'type': 'product',
+        'id': '1'
+    },
+    'test2': {
+        'type': 'product',
+        'id': '2'
+    }
+};
+
+routes['/'] = routes['test1'];
+
+app.get('/:ressource?', function (req, res) {
+    var ressource = req.params.ressource || '/';
+    var route = routes[ressource];
+
+    if (!route) {
+        //404
+        res.end();
+        return;
+    }
+
+    var controller, template;
+    if (route.type === 'product') {
+        controller = productcontroller;
+        template = producttemplate;
+    } else {
+        //404
+        res.end();
+        return;
+    }
+
+    var stream = MustacheEngine.compileAndRender(template, controller.getData(route.id));
     stream.pipe(res);
 });
 
