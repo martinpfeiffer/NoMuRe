17a5d5a524f27a1db164322f2cc94062bbed8165
Martin Pfeiffer <martinp.dev@googlemail.com>
2014-01-24 11:53:06 +0100

    implement first simple controller

---
+++ controller/product.js
@@ -0,0 +1,7 @@
+var productMock = '{"price": 123.45,"name": "Fancy Product","description": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet","instock": true}';
+
+exports.getData = function () {
+	var product = JSON.parse(productMock);
+	product.price = product.price + ' €';
+	return product;
+};
--- index.js
+++ index.js
@@ -3,19 +3,14 @@ var MustacheEngine = require('mu2'); // mu2 is a fast mustache engine
 
 MustacheEngine.root = 'templates';
 
-var staticFile = 'product.html';
-var productMock = {
-    'price': '123,45 €',
-    'name': 'Fancy Product',
-    'description': 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet',
-    'instock': true
-};
+var template = 'product.html';
+var productcontroller = require('./controller/product');
 
 var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    var stream = MustacheEngine.compileAndRender(staticFile, productMock);
+    var stream = MustacheEngine.compileAndRender(template, productcontroller.getData());
     stream.pipe(res);
 });
 
