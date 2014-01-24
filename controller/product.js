4cf9fdbdb3f3cd61dc246140d0772d57e7bdc509
Martin Pfeiffer <martinp.dev@googlemail.com>
2014-01-24 12:54:13 +0100

    implementing REST API

--- controller/product.js
+++ controller/product.js
@@ -1,11 +1,8 @@
-productdata = {
-	'1': '{"price": 123.45,"name": "Fancy Product","description": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet","instock": true}',
-	'2': '{"price": 12.45,"name": "Fancy Product II","description": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet","instock": false}'
-};
+var REST = require('../rest');
 
 exports.getData = function (id) {
-	var data = productdata[id] || '{}';
-	var product = JSON.parse(data);
+	var product = REST.get('/products/' + id);
+
 	product.price = product.price + ' €';
 	return product;
 };
