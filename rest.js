d5b25e4a50324b49293a53466fc52cb5346f5d1c
Wolfgang Loch <wloch@epages.de>
2014-01-24 14:21:16 +0100

    Improved error handling, print port number on startup

--- rest.js
+++ rest.js
@@ -1,8 +1,13 @@
-var ressources = {
+var resources = {
     '/products/1': '{"price": 123.45,"name": "Fancy Product","description": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet","instock": true}',
     '/products/2': '{"price": 12.45,"name": "Fancy Product II","description": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet","instock": false}'
 };
 
 exports.get = function (url) {
-    return JSON.parse(ressources[url] || '{}');
+	var data = resources[url];
+	if (!data) {
+		throw "resource " + url +" not found";
+	}
+	
+    return JSON.parse(data);
 };
